fa3647915e2859aa4dea2b5e175165597663be1f
Tim Whitaker <tmwhtkr@gmail.com>
2013-06-18 21:12:39 -0700

    submit post now works

--- client/managers/posts/post_submit.js
+++ client/managers/posts/post_submit.js
@@ -5,7 +5,8 @@ Template.postSubmit.events({
         var post = {
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val(),
-            description: $e.target).find('[name=description]').val()
+            description: $(e.target).find('[name=description]').val(),
+            price: $(e.target).find('[name=price]').val()
         }
 
         Meteor.call('post', post, function(error, id) {
--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,32 @@
 Posts = new Meteor.Collection('posts');
+
+Meteor.methods({
+    post: function(postAttributes) {
+        var user = Meteor.user();
+        
+        if (!user) 
+            throw new Meteor.Error(401, "You need to login to post new stories!");
+
+        if (!postAttributes.title)
+            throw new Meteor.Error(422, 'Please fill in a headline');
+
+        if (!postAttributes.description)
+            throw new Meteor.Error(422, 'Please write a description');
+
+        if (!postAttributes.price)
+            throw new Meteor.Error(422, 'Please name a price');
+
+        if (!postAttributes.url)
+            throw new Meteor.Error(422, 'Please enter a url');
+
+        var post = _.extend(_.pick(postAttributes, 'url', 'title', 'description', 'price'), {
+            userId: user._id,
+            author: user.username,
+            submitted: new Date().getTime(),
+            commentsCount: 0
+        });
+
+        var postId = Posts.insert(post);
+        return postId;
+    }
+});
