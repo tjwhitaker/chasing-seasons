003878385e60073c90099de4504be5ed494e0488
Tim Whitaker <tmwhtkr@gmail.com>
2013-06-28 14:54:33 -0700

    added permissions for users/posts/comments/categories.

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,51 @@
+Meteor.startup(function() {
+    Meteor.users.allow({
+        insert: function(userId, doc) {
+            return canCreateUser(userId, doc);
+        },
+        update: function(userId, doc, fields, modifier) {
+            return canEditUser(userId, doc);
+        },
+        remove: function(userId, doc) {
+            return canRemoveUser(userId, doc);
+        }
+    });
+    Meteor.posts.allow({
+        insert: function(userId, doc) {
+            return canPost(userId, doc);
+        },
+        update: function(userId, doc, fields, modifier) {
+            return canEditPost(userId, doc);
+        },
+        remove: function(userId, doc) {
+            return canRemovePost(userId, doc);
+        }
+    });
+    Meteor.comments.allow({
+        insert: function(userId, doc) {
+            return canComment(userId, doc);
+        },
+        update: function(userId, doc, fields, modifier) {
+            return canEditComment(userId, doc);
+        },
+        remove: function(userId, doc) {
+            return canRemoveComment(UserId, doc);
+        }
+    });
+    Meteor.categories.allow({
+        insert: function(userId, doc) {
+            return false;
+        },
+        update: function(userId, doc, fields, modifier) {
+            return false;
+        },
+        remove: function(userId, doc) {
+            return false;
+        }
+    });
+});
+
+
 Meteor.publish('allUsers', function() {
     //if admin, publish all fields
     if (this.userId && isAdmin(this.userId)) {
