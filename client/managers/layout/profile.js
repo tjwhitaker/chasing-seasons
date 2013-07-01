935cd7a1fe367da814639fef2d77f3dc506c7bf7
Tim Whitaker <tmwhtkr@gmail.com>
2013-07-01 15:25:36 -0700

    added an each with index helper. Profile now shows items in columns of 3.

--- client/managers/layout/profile.js
+++ client/managers/layout/profile.js
@@ -1,5 +1,17 @@
 Template.profile.helpers({
 	savedPosts: function() {
 		return Meteor.user().profile.savedPosts;
+	},
+	isLast: function(index) {
+		return index%3 ? false : true;
+	},
+	eachWithIndex: function(array, fn) {
+		var buffer = '';
+		for (var i=0, j=array.length; i < j; i++) {
+			var item = array[i];
+			item.index = i+1;
+			buffer += fn(item);
+		}
+		return buffer;
 	}
 });
