e1a1b4d87cc3debdcecc2d9fda119aa7329e88f8
Tim Whitaker <tmwhtkr@gmail.com>
2013-07-11 16:50:39 -0700

    starting fixture data.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,14 +1,38 @@
 if (Posts.find().count() === 0) {
-    var cats = ['Air', 'Dirt', 'Mountain', 'Snow', 'Street', 'Water'];
-    for (i=0; i < 20; i++) {
-        Posts.insert({
-            title: 'Test Post #' + i,
-            url: 'http://www.tim-whitaker.com',
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut libero tortor, consequat vel felis sed, iaculis tincidunt sem. Phasellus hendrerit elementum luctus. Mauris fringilla pharetra fringilla. Nam lacus risus, tempor eu aliquet sed, mattis et metus. Aliquam non est aliquet nisi sollicitudin fringilla. Sed condimentum ornare massa sit amet dictum. Morbi volutpat mi sed justo eleifend, pretium placerat dui accumsan. Nullam tristique nisi eu turpis iaculis commodo. Phasellus eget ante id enim iaculis luctus quis eget tellus. Nunc faucibus pellentesque accumsan. Proin magna nulla, congue a nulla nec, sagittis facilisis orci. Curabitur eget consectetur dui.",
-            price: Math.round(Random.fraction()*100),
-            commentsCount: 0,
-            image: '/fixtureImages/' + i%10 + '.jpg',
-            categories: cats[Math.floor(Math.random()*cats.length)]
-        });
-    }
+	Posts.insert({
+		title: 'Sun Rocket Solar Kettle',
+		url: 'http://www.sunkettle.com',
+		price: '60',
+		description: "The sun rocket is the perfect way to heat and boil your water using only the power of the sun. It's portable, convenient, and sleek. Check it out.",
+		commentsCount: 0,
+		image: '/images/kettle.jpg',
+		categories: 'Camp'
+	});
+	Posts.insert({
+		title: 'Loaded Vanguard Longboard',
+		url: 'www.loadedboards.com/boards/vanguard',
+		price: '311',
+		description: "The vanguard is one sick longboard. Loaded builds all of their boards with an unsurpassed attention to detail. The quality really shines through in the vanguard. If you are looking for a fun, flexy longboard to get you to work or school, or to shred the local hills on your free time, then this is the board for you.",
+		commentsCount: 0,
+		image: '/images/vanguard.jpg',
+		categories: 'Street'
+	});
+	Posts.insert({
+		title: 'Stash Waterproof Pocket Shorts',
+		url: 'http://www.kickstarter.com/projects/1861072211/stash-waterproof-pocket-shorts',
+		price: '80',
+		description: "Never worry about forgetting things in your pocket while you're out in the water again. Stash Incorparated has developed a new waterproof pocket that will allow you to safely take your cell phone, wallet, and keys with you wherever you want to take them.",
+		commentsCount: 0,
+		image: '/images/stash_shorts.jpg',
+		categories: 'Surf'
+	});
+	Posts.insert({
+		title: 'Airblaster Ninja Suit',
+		url: 'http://store.myairblaster.com/t/category/mens/ninjasuits',
+		price: '64-152',
+		description: "Airblaster is revolutionizing the long underwear game with the ninja suits. The ninja suit represents the apex of function, and form. One piece design seals out snow, while the 7-piece hood allows you to regulate your temperture whilst shredding. See what it's like to feel like a ninja on the snow.",
+		commentsCount: 0,
+		image: '/images/ninja_suit.jpg',
+		categories: 'Snow'
+	});
 }
