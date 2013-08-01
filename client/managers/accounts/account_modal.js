69176839ac3e22e2cabfbe0d32f0d81dc078d9d6
Tim Whitaker <tmwhtkr@gmail.com>
2013-08-01 16:35:41 -0700

    added keypress event for login/register

--- client/managers/accounts/account_modal.js
+++ client/managers/accounts/account_modal.js
@@ -14,5 +14,23 @@ Template.accountModal.events = {
             $('.account-modal').hide();
             $('.account-modal .register').hide();
         });
+    },
+    'keypress input': function(event) {
+        if (event.which ===13) {
+            var username = $('.username-field').val();
+            var password = $('.password-field').val();
+            if ($('.login').is(':visible')) {
+                Meteor.loginWithPassword(username, password, function() {
+                    $('.account-modal').hide();
+                    $('.account-modal .login').hide();
+                });
+            }
+            else if ($('.register').is(':visible')) {
+                Accounts.createUser({username:username, password:password}, function() {
+                    $('.account-modal').hide();
+                    $('.account-modal .register').hide();
+                });
+            }
+        }
     }
 };
